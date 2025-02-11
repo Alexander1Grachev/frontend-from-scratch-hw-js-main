39e503ca457be33046e3c1fd456ed984d84d34f5
Alexander1Grachev <alexander1grchv@gmail.com>
2025-02-11 15:57:26 +0300

    lesson-03/03.js.

--- lesson-03/03.js
+++ lesson-03/03.js
@@ -1,21 +1,24 @@
 // Разработайте функцию findLargest, которая принимает три числа и возвращает наибольшее из них.
 
 let largestNumb = 0;
-findLargest(888, 6, 32)
+findLargest(8, 8, 8)
 function findLargest(number_1, number_2, number_3) {
     switch (true) {
         case number_1 > (number_2 && number_3):
             largestNumb = number_1
             return largestNumb
-            
+
         case number_2 > (number_1 && number_3):
             largestNumb = number_2
             return largestNumb
-            
+
         case number_3 > (number_1 && number_2):
             largestNumb = number_3
             return largestNumb
-            
+
+        case number_3 === (number_1 && number_2):
+            largestNumb = number_3
+            return largestNumb
     }
 }
 console.log(largestNumb)
